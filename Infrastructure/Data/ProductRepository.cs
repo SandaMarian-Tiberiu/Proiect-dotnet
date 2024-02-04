3e208873285801f57fea3346093cb9d7fc51a6eb
Sanda Marian-Tiberiu <tiberiusanda@gmail.com>
2024-02-05 00:19:48 +0200

    Generic repo + Specification

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -34,7 +34,7 @@ namespace Infrastructure.Data
 
         public async Task<IReadOnlyList<ProductType>> GetProductTypesAsync()
         {
-            return await _context.ProductTypes.ToListAsync();;
+            return await _context.ProductTypes.ToListAsync();
         }
     }
 }
